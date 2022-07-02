7f77841b916a52a4e022b0419a33122360241a4c
Ishmam Ruhan <ishmamruhan@gmail.com>
2022-07-02 13:07:57 +0600

    Added property configurations and Configuration package init

---
+++ src/main/java/com/teaminvincible/ESchool/Configurations/BeanConfig.java
@@ -0,0 +1,7 @@
+package com.teaminvincible.ESchool.Configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+}
---
+++ src/main/java/com/teaminvincible/ESchool/Configurations/Master/AppInitialization.java
@@ -0,0 +1,15 @@
+package com.teaminvincible.ESchool.Configurations.Master;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class AppInitialization {
+
+    @PostConstruct
+    public void init(){
+
+    }
+
+}
